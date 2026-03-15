ad785ecf71f9fff11246198328b515042647fd36
Peterson Ruivo <peterson_r_alves@estudante.sesisenai.org.br>
2026-03-15 16:01:00 -0300

    correção na lógica de erro e sucesso ao se inscrever no curso

--- projeto/inscricao.php
+++ projeto/inscricao.php
@@ -18,13 +18,23 @@ AND curso_id = '$curso_id'
 $result = mysqli_query($conexao,$sql_verifica);
 
 if(mysqli_num_rows($result) == 0){
-
+    // Não está inscrito, faz o insert
     $sql = "
     INSERT INTO inscricoes (usuario_id, curso_id)
     VALUES ('$usuario_id','$curso_id')
     ";
-
+    
     mysqli_query($conexao,$sql);
+
+    // CRIA A MENSAGEM DE SUCESSO NA SESSÃO
+    $_SESSION['msg_texto'] = "Inscrição realizada com sucesso! Bons estudos.";
+    $_SESSION['msg_tipo'] = "sucesso"; 
+
+} else {
+    // Já estava inscrito
+    // CRIA A MENSAGEM DE AVISO NA SESSÃO
+    $_SESSION['msg_texto'] = "Você já está inscrito neste curso!";
+    $_SESSION['msg_tipo'] = "aviso";
 }
 
 header("Location: cursos.php");
